0c1b6d3e2f55f480b5b28a2d3deb4e7ea178b25b
Jumcee <futhmah456@gmail.com>
2022-09-08 19:44:14 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,9 @@
 /**
  * main - main block
  * Return: 0
- * /
- int main(void)
- {
- printf("Size of a char: %i byte(s)\n", sizeof(char));
- printf("Size of an int: %i byte(s)\n", sizeof(int));
- printf("Size of a long int: %i byte(s)\n", sizeof(long int));
- printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
- printf("Size of a float: %i byte(s)\n", sizeof(float));
- return (0);
- }
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
