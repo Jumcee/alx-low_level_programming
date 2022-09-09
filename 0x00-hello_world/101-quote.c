c00b7708b916bebebc24d79e6c6b15b98b947a1d
Jumcee <futhmah456@gmail.com>
2022-09-08 20:14:13 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,18 @@
 #include <unistd.h>
 
 /**
- * main - Prints a text
+ * main -Entry point
  *
  * Return: Always 1 (Success)
  */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar,
 
-			2015-10-19\n", 59);
+int main(void)
+
+{
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
+
 	return (1);
 }
 
