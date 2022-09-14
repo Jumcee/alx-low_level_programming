5fafa187c5e22667b724da6b0a3edc0914686a24
Jumcee <futhmah456@gmail.com>
2022-09-14 02:39:54 -0700

    0-purchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,24 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+ * * main - prints _putchar
+ * *
+ * * Description: prints _putchar
+ * *
+ * * Return: 0 (Success)
+ * */
+
+int main(void)
 {
-	return (write(1, &c, 1));
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include<stdio.h>
-
-int _putchar(char c);
+int _putchar (char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -16,6 +11,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endif /* MAIN_H */
 
