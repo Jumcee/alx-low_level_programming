f7ce081af2b1e2337340ae233b2868e407b2fcb4
Jumcee <futhmah456@gmail.com>
2022-09-14 17:36:57 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,63 @@
-#include "main.h"
-
-/**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
- */
-
-void print_times_table(int n)
-{
-
-	int i, j, k;
-
-	if (n >= 0 && n <= 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
-	}
-}
+vi 100-times_table.c 
+#include"main.h" 
+ 
+/** 
+ *  * print_times_table - prints time table of n 
+ *   * 
+ *    * @n: takes number input 
+ *    */ 
+ 
+void print_times_table(int n) 
+{ 
+	 int prod, mult, num; 
+	  
+	  if (n <= 15 && n >= 0) 
+		   { 
+			     for (num = 0; num <= n; ++num) 
+				       { 
+					          _putchar(48); 
+						     for (mult = 1; mult <= n; ++mult) 
+							        { 
+									    _putchar(','); 
+									        _putchar(' '); 
+										 
+										    prod = num * mult; 
+										     
+										        if (prod <= 9) 
+												     _putchar(' '); 
+											    if (prod <= 99) 
+												         _putchar(' '); 
+											     
+											        if (prod >= 100) 
+													    { 
+														         _putchar((prod / 100) + 48); 
+															      _putchar((prod / 10) % 10 + 48); 
+															          } else if (prod <= 99 && prod >= 10) 
+																	       _putchar((prod / 10) + 48); 
+																      _putchar((prod % 10) + 48); 
+																 
+#include"main.h" 
+ 
+/** 
+ * main - Entry point 
+ * 
+ * Description: computes the sum of all the multiples of 3 or 5 
+ * below 1024 (excluded) 
+ *  
+ * Return: Always 0 (Success) 
+ */ 
+ 
+int main(void) 
+{ 
+	 int sum, num; 
+	  
+	  for (num = 0; num < 1024; ++num) 
+		   { 
+			     if ((num % 3 == 0) || (num % 5 == 0)) 
+				        sum += num; 
+			      } 
+	   printf("%d\n", sum); 
+	    
+	    return (0); 
+} 
+ 
