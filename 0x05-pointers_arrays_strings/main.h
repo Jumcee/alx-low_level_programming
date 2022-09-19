5477aff7f3b02e1d1c5c8ec2a2c0db98778d4432
Jumcee <futhmah456@gmail.com>
2022-09-19 14:02:42 -0700

    Task 3,4,6,7

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * void prototypes()
- * int prototypes()
- */
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -17,6 +9,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
-
-#endif /* MAIN_H */
 
