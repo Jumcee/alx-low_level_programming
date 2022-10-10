994f57ea1c3edb750529332b3daf158fc1d7f0d7
Jumcee <futhmah456@gmail.com>
2022-10-10 14:42:12 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* struct dog - new type for dog
+* @name: char meember
+* @age: Float member
+* @owner: char argument
+*
+* Description: Structured type for Dog
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+
+
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
